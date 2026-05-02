45750abfa50d9861c9174e61c0c6af462de32c23
Purushottam Kumar <140494301+purushottam7660@users.noreply.github.com>
2026-05-03 02:59:43 +0530

    Create cookies.py

---
+++ cookies.py
@@ -0,0 +1,8 @@
+NAUKRI_COOKIES = {
+    "is_login": "1",
+    "nauk_sid": "dda6fbb42c6f4f9fa855c87a1b4122bd",
+    "nauk_otl": "dda6fbb42c6f4f9fa855c87a1b4122bd",
+    "PHPSESSID": "dhaqfbknq3c2sj0nrf518n5brl",
+    "nauk_ps": "default",
+    "J": "0"
+}
