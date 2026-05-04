2ad1545761e3b446e8fb5e5773d0f1a007894af4
Purushottam Kumar <140494301+purushottam7660@users.noreply.github.com>
2026-05-04 15:44:37 +0530

    Update naukri.py

--- naukri.py
+++ naukri.py
@@ -50,12 +50,14 @@ def get_driver():
     options = Options()
 
     # Reduce "access denied"
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--user-data-dir=/tmp/chrome-profile")
-
-    options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--remote-debugging-port=9222")
+
+    # Stability
+    options.add_argument("--window-size=1920,1080")
 
     # User-Agent spoof
     options.add_argument(
